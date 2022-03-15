d65f48bcacecdeab9348457af138ca733454410a
Vinícius Garcia <vingarcia00@gmail.com>
2022-03-15 18:38:54 -0300

    Improve on the example by adding an html directory

--- rootfs.go
+++ rootfs.go
@@ -4,5 +4,8 @@ import (
 	"embed"
 )
 
-//go:embed config/*
+// You can add more subdirectories here, by separating them with spaces
+// like we do for the config and html directories below:
+//
+//go:embed config/* html/*
 var RootFS embed.FS
--- subpackage/file_test.go
+++ subpackage/file_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestHelloWorld(t *testing.T) {
+func TestOpenConfig(t *testing.T) {
 	configFile, err := rootpkg.RootFS.ReadFile("config/env")
 	if err != nil {
 		t.Fatalf("unable to open config/env file: %s", err)
@@ -13,5 +13,16 @@ func TestHelloWorld(t *testing.T) {
 
 	if string(configFile) != "FOO=bar\n" {
 		t.Fatalf("config file contents differ from expected: %s", string(configFile))
+	}
+}
+
+func TestOpenHTML(t *testing.T) {
+	configFile, err := rootpkg.RootFS.ReadFile("html/index.html")
+	if err != nil {
+		t.Fatalf("unable to open html/index.html file: %s", err)
+	}
+
+	if string(configFile) != "<body>example</body>\n" {
+		t.Fatalf("html file contents differ from expected: %s", string(configFile))
 	}
 }
